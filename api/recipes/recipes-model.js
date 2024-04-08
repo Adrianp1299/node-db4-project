a6ab23f5362747602bb209d71cba9770b989a655
Adrian Perez <adrianp1299@gmail.com>
2024-04-08 00:44:45 -0600

    wip

---
+++ api/recipes/recipes-model.js
@@ -0,0 +1,7 @@
+function getRecipeById(recipe_id) {
+    return Promise.resolve('awesome recip with ID')
+}
+
+module.exports = {
+    getRecipeById
+}
--- api/server.js
+++ api/server.js
@@ -7,4 +7,8 @@ server.use(express.json())
 
 server.use('/api/recipes', recipesRouter)
 
+router.use('*', (req, res) => {
+    res.json({ api: 'up' })
+})
+
 module.exports = server
